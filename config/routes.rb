d2e97411f3c414a07a852e9975f662fb6b02e66a
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-03-06 02:31:00 +0000

    public側ルーティング製作中

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+  
+  # 一般ユーザーで使用するルーティング
+  scope module: :public do
+    root to: 'homes#top'
+    
+    resources :items, only: [:index, :show] do
+      resources :comments, only: [:new, :index, :create]
+    end
+    
+    resource :users, only: [:show, :edit, :update] do
+      get :check, on: :collection
+      patch :withdrawl, on: :collection
+    end
+    
+    get "search" => "searches#search"
+    resources :operation_systems, only: [:show]
+    resources :central_processing_units, only: [:show]
+    resources :certifications, only: [:show]
+    resources :specs, only: [:show]
+    resources :mekars, only: [:show]
+    resources :capacities, only: [:show]
+  end
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
