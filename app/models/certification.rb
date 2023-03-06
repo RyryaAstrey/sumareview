ff174c37799ba222341e444da5bd07c01a40c063
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-03-06 06:49:38 +0000

    [Add]authenticationsテーブル追加

--- app/models/certification.rb
+++
@@ -1,2 +0,0 @@
-class Certification < ApplicationRecord
-end
---
+++ db/migrate/20230306064403_create_authentications.rb
@@ -0,0 +1,10 @@
+class CreateAuthentications < ActiveRecord::Migration[6.1]
+  def change
+    create_table :authentications do |t|
+      
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
