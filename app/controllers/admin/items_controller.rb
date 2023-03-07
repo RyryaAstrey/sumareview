b7e98e15646bcf56afb4564d3e328d76d7f7a7dc
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-03-07 08:40:17 +0000

    [FEATURE]管理者新規記事投稿機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,16 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
   end
 
   def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to admin_items_path
+    else
+      flash[:danger] = "入力内容に不備があります。<br>・#{@product.errors.full_messages.join('<br>・')}"
+      redirect_to new_admin_item_path
+    end
   end
 
   def show
