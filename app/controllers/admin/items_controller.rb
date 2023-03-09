89d177f874ac19c517ff1af7d826219190715f03
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-03-09 02:32:06 +0000

    [FINISH]管理者記事新規登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,11 +5,22 @@ class Admin::ItemsController < ApplicationController
 
   def create
     item = Item.new(item_params)
-    if item.save
-      redirect_to admin_items_path
+    # 投稿ボタンを押下した場合
+    if params[:post]
+      if item.save(context: :publicize)
+        redirect_to request.referer
+      else
+        flash[:danger] = "入力内容に不備があります。<br>・#{@product.errors.full_messages.join('<br>・')}"
+        redirect_to new_admin_item_path
+      end
+    # 下書きボタンを押下した場合
     else
-      flash[:danger] = "入力内容に不備があります。<br>・#{@product.errors.full_messages.join('<br>・')}"
-      redirect_to new_admin_item_path
+      if item.update(is_draft: true)
+        redirect_to admin_items_path
+      else
+        flash[:danger] = "入力内容に不備があります。<br>・#{@product.errors.full_messages.join('<br>・')}"
+        redirect_to new_admin_item_path
+      end
     end
   end
 
@@ -20,5 +31,38 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(
+      :operation_system_id,
+      :central_processing_unit_id,
+      :certification_id,
+      :spec_id,
+      :maker_id,
+      :capacity_id,
+      :name,
+      :explanation,
+      :strength,
+      :weakness,
+      :core,
+      :main_camera,
+      :in_camera,
+      :mobile_wallet,
+      :release_date,
+      :price,
+      :battery,
+      :display,
+      :weight,
+      :height,
+      :thickness,
+      :memory,
+      :waterploof,
+      :wireless_chage,
+      :near_field_communication,
+      :is_draft
+      )
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   # enum
   enum waterploof: { "water_true": 0, "water_false": 1 }
   enum near_field_communication: { "near_true": 0, "near_false": 1 }
+  enum wireless_chage: { "wireless_true": 0, "wireless_false": 1 }
 end
