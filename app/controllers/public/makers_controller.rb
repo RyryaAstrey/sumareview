7d4ad8f76ca36238d819d92c8787261e16774b00
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-03-23 02:16:29 +0000

    [hotfix]カテゴリ検索における点数ソート機能修正

--- app/controllers/public/makers_controller.rb
+++ app/controllers/public/makers_controller.rb
@@ -5,8 +5,13 @@ class Public::MakersController < ApplicationController
     @maker = Maker.find(params[:id])
     if params[:latest]
       @maker_items = @maker.items.latest.where(is_draft: false).page(params[:page]).per(10)
+    # 平均点昇順
     elsif params[:score_count]
-      @maker_items = @maker.items.score_count.where(is_draft: false).page(params[:page]).per(10)
+      @maker_items = @maker.items.where(is_draft: false).each do |item| # 非公開記事を除く記事を取得し、配列にする
+      end
+      @maker_items = @maker_items.sort_by { |item| item.review_score_average }.reverse # sort_byで平均点順に並び直し、reverseで昇順になった配列を作る（review_score_averageはモデルで定義）。
+      @maker_items = Kaminari.paginate_array(@maker_items).page(params[:page]).per(10) # @itemsは現状配列のため、Kaminari.pagenate_arrayを使ってページネーションを作る。
+    # --ここまで平均点昇順--
     elsif params[:row_cost]
       @maker_items = @maker.items.row_cost.where(is_draft: false).page(params[:page]).per(10)
     else
--- app/controllers/public/operation_systems_controller.rb
+++ app/controllers/public/operation_systems_controller.rb
@@ -5,8 +5,13 @@ class Public::OperationSystemsController < ApplicationController
     @operation_system = OperationSystem.find(params[:id])
     if params[:latest]
       @operation_system_items = @operation_system.items.latest.where(is_draft: false).page(params[:page]).per(10)
+    # 平均点昇順
     elsif params[:score_count]
-      @operation_system_items = @operation_system.items.score_count.where(is_draft: false).page(params[:page]).per(10)
+      @operation_system_items = @operation_system.items.where(is_draft: false).each do |item| # 非公開記事を除く記事を取得し、配列にする
+      end
+      @operation_system_items = @operation_system_items.sort_by { |item| item.review_score_average }.reverse # sort_byで平均点順に並び直し、reverseで昇順になった配列を作る（review_score_averageはモデルで定義）。
+      @operation_system_items = Kaminari.paginate_array(@operation_system_items).page(params[:page]).per(10) # @itemsは現状配列のため、Kaminari.pagenate_arrayを使ってページネーションを作る。
+    # --ここまで平均点昇順--
     elsif params[:row_cost]
       @operation_system_items = @operation_system.items.row_cost.where(is_draft: false).page(params[:page]).per(10)
     else
