af96b350e1d33e59af02b71b471a084d36d1feae
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-03-26 01:38:02 +0000

    [FINISH]リーダブルコード

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -20,7 +20,7 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @comments = @item.comments.order('id DESC')
+    @comments = @item.comments.order('id DESC').limit(5)
     @images = @item.images
   end
   
